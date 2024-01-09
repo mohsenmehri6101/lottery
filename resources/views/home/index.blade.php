37832dea80d9536a16bc32bbd9a8154b418e0b0d
mohsenmehri6101 <mohsen.mehri6101@gmail.com>
2024-01-10 00:07:34 +0330

    refactor todo.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>title</title>
-    <link rel="stylesheet" href='{{ url("assets/index-28ac7949.css") }}'>
+    <link rel="stylesheet" href='{{ url("assets/index-cf13db21.css") }}'>
 </head>
 <body>
 <div id="app"></div>
-<script type="module" crossorigin src="{{ url('assets/index-96dd49ab.js') }}"></script>
+<script type="module" crossorigin src="{{ url('assets/index-2e1c690f.js') }}"></script>
 </body>
 </html>
