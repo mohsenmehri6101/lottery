6626e5d0d6a869c968710b846f873ea07be4f61d
mohsenmehri6101 <mohsen.mehri6101@gmail.com>
2024-01-14 20:14:04 +0330

    add to do.

--- Modules/Payment/Database/Migrations/2022_10_12_000000_create_transactions_table.php
+++ Modules/Payment/Database/Migrations/2022_10_12_000000_create_transactions_table.php
@@ -1,34 +1,41 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 return new class extends Migration {
     public function up(): void
     {
-        //        Schema::create('transactions', function (Blueprint $table) {
-        //            $table->id();
-        //            $table->string('amount')->nullable()->comment('price');
-        //            $table->text('description')->nullable()->comment('شرح عملیات');
-        //            $table->tinyInteger('specification')->default(0)->nullable()->comment('تشخیص(بدهکار-بستانکار)');
-        //            $table->unsignedBigInteger('user_id')->nullable()->comment('user_id');
-        //            $table->unsignedBigInteger('user_creator')->nullable()->comment('user_creator');
-        //            $table->unsignedBigInteger('user_editor')->nullable()->comment('user_editor');
-        //            $table->timestamps();
-        //            $table->softDeletes();
-        //        });
-        //        Schema::create('transaction_details', function (Blueprint $table) {
-        //            $table->id();
-        //            $table->text('description')->nullable()->comment('شرح عملیات');
-        //            $table->string('amount')->nullable()->comment('amount');
-        //            $table->unsignedBigInteger('transaction_id')->nullable()->comment('transaction_id');
-        //            $table->unsignedBigInteger('user_id')->nullable()->comment('user_id');
-        //            $table->tinyInteger('status')->nullable()->default(0/*0*/)->comment('');
-        //        });
+        // every transaction two.
+                Schema::create('transactions', function (Blueprint $table) {
+                    $table->id();
+                    // date.
+                    $table->unsignedBigInteger('user_destination')->nullable()->comment('user_destination');
+                    $table->unsignedBigInteger('user_resource')->nullable()->comment('user_resource');
+                    $table->string('price')->nullable()->comment('price');
+                    $table->text('description')->nullable()->comment('شرح عملیات');
+                    $table->tinyInteger('specification')->default(0)->nullable()->comment('تشخیص(بدهکار-بستانکار)');
+                    $table->unsignedBigInteger('user_id')->nullable()->comment('user_id');
+                    $table->unsignedBigInteger('user_creator')->nullable()->comment('user_creator');
+                    $table->unsignedBigInteger('user_editor')->nullable()->comment('user_editor');
+                    $table->timestamps();
+                    $table->softDeletes();
+                });
+
+                Schema::create('transaction_details', function (Blueprint $table) {
+                    $table->id();
+                    $table->text('description')->nullable()->comment('شرح عملیات');
+                    $table->string('amount')->nullable()->comment('amount');
+                    $table->unsignedBigInteger('transaction_id')->nullable()->comment('transaction_id');
+                    $table->unsignedBigInteger('user_id')->nullable()->comment('user_id');
+                    $table->tinyInteger('status')->nullable()->default(0/*0*/)->comment('');
+                });
     }
 
     public function down(): void
     {
-        // Schema::dropIfExists('transaction_details');
-        // Schema::dropIfExists('transactions');
+         Schema::dropIfExists('transaction_details');
+         Schema::dropIfExists('transactions');
     }
 };
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>عنوان</title>
-    <link rel="stylesheet" href='{{ url("assets/index-52e7c913.css") }}'>
+    <link rel="stylesheet" href='{{ url("assets/index-66ad168b.css") }}'>
 </head>
 <body>
 <div id="app"></div>
-<script type="module" crossorigin src="{{ url('assets/index-593f7bfd.js') }}"></script>
+<script type="module" crossorigin src="{{ url('assets/index-7b35a8fa.js') }}"></script>
 </body>
 </html>
