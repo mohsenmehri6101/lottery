ec12a0f757cfa0195d1e6bd909e2798242e3962a
mohsenmehri6101 <mohsen.mehri6101@gmail.com>
2024-01-19 14:47:52 +0330

    123

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Modules\Gym\Services\GymService;
-
 class HomeController extends Controller
 {
     public function index()
     {
-        /** @var GymService $gymService */
-        $gymService = resolve('GymService');
-        return $gymService->getInitializeRequestsSelectors(['withs'=>['gyms','tags','categories','sports','attributes','keywords','cities','provinces','gender_acceptances']]);
         return view('home.index');
     }
 
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>عنوان</title>
-    <link rel="stylesheet" href='{{ url("assets/index-5eb29a49.css") }}'>
+    <link rel="stylesheet" href='{{ url("assets/index-06e3a481.css") }}'>
 </head>
 <body>
 <div id="app"></div>
-<script type="module" crossorigin src="{{ url('assets/index-0f3e8fe0.js') }}"></script>
+<script type="module" crossorigin src="{{ url('assets/index-8f802c9e.js') }}"></script>
 </body>
 </html>
