807aa11183f5ffbe8fc3cd3268001a2fce00feea
mohsenmehri6101 <mohsen.mehri6101@gmail.com>
2024-02-04 17:30:53 +0330

    Update 2022_10_12_000001_create_factors_table.php

--- Modules/Payment/Database/Migrations/2022_10_12_000001_create_factors_table.php
+++ Modules/Payment/Database/Migrations/2022_10_12_000001_create_factors_table.php
@@ -6,12 +6,13 @@ use Illuminate\Support\Facades\Schema;
 use Modules\Payment\Entities\Factor;
 
 return new class extends Migration {
-    public function up(): void
-    {
+    
+    public function up(): void {
         Schema::create('factors', function (Blueprint $table) {
             $table->id();
             $table->string('code')->unique()->comment('code');
-            $table->decimal('total_price', 15, 3)->default(0);
+            $table->decimal('total_price', 15, 3)->default(0)->comment('مجموع قیمت بدون مالیات'); // مجموع قیمت بدون مالیات
+            $table->decimal('total_price_vat', 15, 3)->default(0)->comment('مجموع قیمت شامل مالیات'); // مجموع قیمت با مالیات
             $table->tinyInteger('status')->nullable()->default(Factor::status_unknown/*0*/)->comment('وضعیت پرداخت');
             $table->unsignedBigInteger('user_id')->nullable()->comment('کاربری که فاکتور براش ایجاد شده');
             $table->unsignedBigInteger('user_creator')->nullable()->comment('user_creator');
