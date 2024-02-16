f996acedd3d37d39448b7c176471bc29c5823647
mohsenmehri6101 <mohsen.mehri6101@gmail.com>
2024-02-16 12:10:09 +0330

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,8 +6,8 @@
     <title>
         رزر آنلاین باشگاه ورزشی
     </title>
-    <script type="module" crossorigin src="{{ url('/assets/index-ddeb9a30.js') }}"></script>
-    <link rel="stylesheet" href='{{ url("assets/index-70036342.css") }}'>
+    <script type="module" crossorigin src="{{ url('/assets/index-7aa6bf33.js') }}"></script>
+    <link rel="stylesheet" href='{{ url("assets/index-3ebc128b.css") }}'>
 </head>
 <body>
 <div id='app' ></div>
