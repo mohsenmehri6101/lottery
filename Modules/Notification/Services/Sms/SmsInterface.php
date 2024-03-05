d197bd1efa79c7244f7290c65fd41aafa1208e7a
mohsenmehri6101 <mohsen.mehri6101@gmail.com>
2024-03-05 21:45:36 +0330

    Update SmsInterface.php

--- Modules/Notification/Services/Sms/SmsInterface.php
+++ Modules/Notification/Services/Sms/SmsInterface.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Notification\Services\Sms;
 
-
 interface SmsInterface
 {
     public static function send_sms(string|int $mobile, string $message = null, bool $throwException = true): bool;
