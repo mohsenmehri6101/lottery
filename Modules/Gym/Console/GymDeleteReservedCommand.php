b6750acdb0653d61c41ad32d44ec80cf9370a8e4
mohsenmehri6101 <mohsen.mehri6101@gmail.com>
2024-03-06 00:14:53 +0330

    Update GymDeleteReservedCommand.php

--- Modules/Gym/Console/GymDeleteReservedCommand.php
+++ Modules/Gym/Console/GymDeleteReservedCommand.php
@@ -1,12 +1,11 @@
 <?php
-
 namespace Modules\Gym\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class GymDeleteReservedCommand extends Command
 {
-
     protected $signature = 'gym:delete-reserved';
 
     protected $description = 'Delete gym reservations with status "reserving" and past "reserved_at" time.';
@@ -18,6 +17,7 @@ class GymDeleteReservedCommand extends Command
 
     public function handle(): void
     {
+        Log::info('he say true');
         deleteReservedWithStatusReserving();
         $this->info('All reserved with status reserving deleted successfully');
     }
